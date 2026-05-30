2b0bb585c4df80d1dfcd2d80236c83570b1a6323
SHARV <sharvofficial2006@gmail.com>
2026-05-30 23:07:37 +0530

    all setup configured

---
+++ src/routes/user.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { app } from "../app.js";
+import { registerUser } from "../controllers/user.controller.js";
+
+const router = Router();
+
+router.route("/register").post(registerUser);
+
+
+export default router;
